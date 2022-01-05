7c55897d04ee162380f69ff7b4aec0110253cb7b
Beyn <erdener.unal@beyn.com.tr>
2022-01-05 14:36:10 +0300

    connected with mongoDb. Created User model.

--- helpers/database/connectDatabase.js
+++ helpers/database/connectDatabase.js
@@ -1,7 +1,12 @@
 const mongoose = require('mongoose');
 
+
+let options = {
+    useNewUrlParser: true
+}
+
 const connectDatabase = () => {
-    mongoose.connect(process.env.MONGO_URI)
+    mongoose.connect(process.env.MONGO_URI, options)
         .then(() => {
             console.log("MongoDb connected.")
         })
---
+++ models/User.js
@@ -0,0 +1,54 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema({
+  name: {
+    type: String,
+    required: [true, "Please provide a name"],
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: [true, "Please try a different email"],
+    match: [
+      /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/,
+      "Please fill a valid email address",
+    ],
+  },
+  role: {
+      type: String,
+      default: "user",
+      enum: ["user", "admin"]
+  },
+  password: {
+      type: String,
+      minlength: [6, "Please provide a password with min length: 6"],
+      required: [true, "Please provide a valid password"],
+      select: false,
+  },
+  createdAt: {
+      type: Date,
+      default: Date.now,
+  },
+  title: {
+      type: String,
+  },
+  about: {
+      type: String
+  },
+  place: {
+      type: String
+  },
+  website: {
+      type: String
+  },
+  profileImage: {
+      type: String,
+      default: "default.jpg"
+  },
+  blocked: {
+      type: Boolean,
+      default: false
+  }
+});
