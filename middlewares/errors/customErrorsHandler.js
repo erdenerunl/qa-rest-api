d41b6d471fccd3a723dc1b538903ee3d89063480
Beyn <erdener.unal@beyn.com.tr>
2022-01-06 22:26:44 +0300

    async error handler added.

--- middlewares/errors/customErrorsHandler.js
+++ middlewares/errors/customErrorsHandler.js
@@ -1,7 +1,8 @@
 const customErrorHandler = (err, req, res, next) => {
     console.log(err);
     res.status(400).json({
-        status: false
+        status: false,
+        message: err.message
     })
 }
 
